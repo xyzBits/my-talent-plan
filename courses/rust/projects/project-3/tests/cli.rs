c425ed784b1660ce3a7a20ca7f0f2af0676ce29c
dongfang <lidf077@gmail.com>
2024-05-24 15:53:20 +0800

    add test

--- courses/rust/projects/project-3/tests/cli.rs
+++ courses/rust/projects/project-3/tests/cli.rs
@@ -1,12 +1,208 @@
+use std::fmt::Debug;
+use std::fs::File;
 use std::process::Command;
+use std::{fs, thread};
+use std::time::Duration;
+
 use assert_cmd::prelude::{CommandCargoExt, OutputAssertExt};
+use predicates::str::contains;
 use tempfile::TempDir;
 
 // `kvs-client` with no args should with a non-zero code
 #[test]
 fn client_cli_no_args() {
     let temp_dir = TempDir::new().unwrap();
+    // cargo_bin Create a command to run a specific binary of the current crate
     let mut cmd = Command::cargo_bin("kvs-client").unwrap();
-    cmd.current_dir(&temp_dir).assert().failure();
+
+    // Sets the working directory for the child process
+    cmd.current_dir(&temp_dir)
+
+        // wrap with an interface for that provides assertions on the output
+        .assert()
+        // ensure the command failed
+        .failure();
+}
+
+#[test]
+fn test_ls() {
+    Command::new("ls")
+        .current_dir("/bin")
+        .spawn()
+        .expect("xxxx");
+}
+
+
+#[test]
+fn client_cli_invalid_get() {
+    let temp_dir = TempDir::new().unwrap();
+    Command::cargo_bin("kvs-client")
+        .unwrap()
+        .args(&["get"])
+        .current_dir(&temp_dir)
+        .assert()
+        .failure();
+
+
+    let res = Command::cargo_bin("kvs-client")
+        .unwrap()
+        .args(&["get"])
+        // Executing the command as a child process and returning a handle to it
+        .spawn();
+
+
+    Command::cargo_bin("kvs-client")
+        .unwrap()
+        .args(&["get", "extra", "field"])
+        .current_dir(&temp_dir)
+        .assert()
+        .failure();
+
+    Command::cargo_bin("kvs-client")
+        .unwrap()
+        .args(&["get", "key", "--addr", "invalid-addr"])
+        .current_dir(&temp_dir)
+        .assert()
+        .failure();
+
+    Command::cargo_bin("kvs-client")
+        .unwrap()
+        .args(&["get", "key", "--addr", "--unknown-flag"])
+        .current_dir(&temp_dir)
+        .assert()
+        .failure();
+}
+
+
+#[test]
+fn client_cli_invalid_set() {
+    let temp_dir = TempDir::new().unwrap();
+
+    Command::cargo_bin("kvs-client")
+        .unwrap()
+        .args(&["set"])
+        .current_dir(&temp_dir)
+        .assert()
+        .failure();
+
+    Command::cargo_bin("kvs-client")
+        .unwrap()
+        .args(&["set", "missing_field"])
+        .current_dir(&temp_dir)
+        .assert()
+        .failure();
+
+    Command::cargo_bin("kvs-client")
+        .unwrap()
+        .args(&["set", "key", "value", "--addr", "invalid-addr"])
+        .current_dir(&temp_dir)
+        .assert()
+        .failure();
+
+    Command::cargo_bin("kvs-client")
+        .unwrap()
+        .args(&["set", "key", "value", "extra_field"])
+        .current_dir(&temp_dir)
+        .assert()
+        .failure();
+
+    Command::cargo_bin("kvs-client")
+        .unwrap()
+        .args(&["set", "key", "--unknown-flag"])
+        .current_dir(&temp_dir)
+        .assert()
+        .failure();
+
+}
+
+#[test]
+fn client_cli_invalid_rm() {
+    let temp_dir = TempDir::new().unwrap();
+
+    Command::cargo_bin("kvs-client")
+        .unwrap()
+        .args(&["rm"])
+        .current_dir(&temp_dir)
+        .assert()
+        .failure();
+
+
+    Command::cargo_bin("kvs-client")
+        .unwrap()
+        .args(&["rm", "extra", "field"])
+        .current_dir(&temp_dir)
+        .assert()
+        .failure();
+
+    Command::cargo_bin("kvs-client")
+        .unwrap()
+        .args(&["rm", "key", "--unknown-flag"])
+        .current_dir(&temp_dir)
+        .assert()
+        .failure();
+
+    Command::cargo_bin("kvs-client")
+        .unwrap()
+        .args(&["rm", "key", "--addr", "invalid-addr"])
+        .current_dir(&temp_dir)
+        .assert()
+        .failure();
+}
+
+
+#[test]
+fn client_cli_invalid_subcommand() {
+    let temp_dir = TempDir::new().unwrap();
+
+    Command::cargo_bin("kvs-client")
+        .unwrap()
+        .args(&["unknown"])
+        .current_dir(&temp_dir)
+        .assert()
+        .failure();
+
+}
+
+// `kvs-client -V` should print the version
+#[test]
+fn client_cli_version() {
+    let temp_dir = TempDir::new().unwrap();
+
+    let mut cmd = Command::cargo_bin("kvs-client").unwrap();
+
+    cmd.args(&["-V"])
+        .current_dir(&temp_dir)
+        .assert()
+        // Ensure the command wrote the expected data to stdout
+        .stdout(contains(env!("CARGO_PKG_VERSION")));
+
+}
+
+
+#[test]
+fn cli_log_configuration() {
+    let temp_dir = TempDir::new().unwrap();
+
+    let stderr_path = temp_dir.path().join("stderr");
+
+    let mut cmd = Command::cargo_bin("kvs-server").unwrap();
+
+    let mut child = cmd
+        .args(&["--engine", "kvs", "--addr", "127.0.0.1:4001"])
+        .current_dir(&temp_dir)
+        .stderr(File::create(&stderr_path).unwrap())
+        .spawn()
+        .unwrap();
+
+    thread::sleep(Duration::from_secs(1));
+    child.kill().expect("server exited before killed");
+
+    let content = fs::read_to_string(&stderr_path).expect("unable to read from stderr file");
+
+    println!("{}", content);
+
+    assert!(content.contains(env!("CARGO_PKG_VERSION")));
+    assert!(content.contains("kvs"));
+    assert!(content.contains("127.0.0.1:4001"));
 
 }
--- courses/rust/projects/project-3/tests/lib_api.rs
+++ courses/rust/projects/project-3/tests/lib_api.rs
@@ -35,7 +35,6 @@ fn test_str_ref() {
 
 #[cfg(test)]
 mod test_iterator {
-
     struct Fibonacci {
         curr: u32,
         next: u32,
@@ -91,5 +90,66 @@ mod test_iterator {
         for x in array.into_iter() {
             println!("{}", x);
         }
+    }
+}
+
+
+#[cfg(test)]
+mod test_serde_json {
+    use std::borrow::Cow;
+    use std::time::Instant;
+    use serde::Deserialize;
+
+    #[derive(Deserialize)]
+    struct UserA {
+        name: String,
+        age: i32,
+        blog: String,
+        addr: String,
+    }
+
+    #[derive(Deserialize)]
+    struct UserB<'a> {
+        name: &'a str,
+        age: i32,
+        blog: &'a str,
+        addr: &'a str,
+    }
+
+    #[derive(Deserialize)]
+    struct UserC<'a> {
+        #[serde(borrow)]
+        name: Cow<'a, str>,
+
+        age: i32,
+
+        #[serde(borrow)]
+        blog: Cow<'a, str>,
+
+        #[serde(borrow)]
+        addr: Cow<'a, str>,
+    }
+
+
+    #[test]
+    fn test_serde() -> serde_json::Result<()> {
+
+        let json = r#"{
+        "name": "rust",
+        "age": 12,
+        "blog": "rust book",
+        "addr": "rust.org"
+        }"#;
+
+
+        let start = Instant::now();
+        for i in 0..100_000 {
+            let user = serde_json::from_str::<UserC>(json)?;
+        }
+
+        println!("time cost: {:?} ms", start.elapsed().as_millis());
+
+        Ok(())
+
     }
 }
